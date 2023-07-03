519f29bd90d0bdfdd924e7269e411331a770c369
Vinicius Faria <viniciusfdj@gmail.com>
2023-07-03 15:06:52 -0300

    Final project 1.0

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,20 @@
 import { Request, Response } from 'express';
+import { Pet } from '../models/pet';
+import { createMenuObject } from '../helpers/createMenuObject';
 
 export const search = (req: Request, res: Response) => {
-  // res.render('pages/search);
+  
+  const query: string = req.query.q as string;
+
+  if(!query)
+    return res.redirect('/');
+
+  const list = Pet.getFromName(query)
+
+  res.render('pages/page', {
+    menu: createMenuObject(''),
+    list,
+    query
+  })
+  
 }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,9 +4,9 @@ import * as SearchController from '../controllers/searchController';
 
 const router = Router();
 
-router.get('/', PageController.home);
-router.get('/dogs', PageController.dogs);
-router.get('/cats', PageController.cats);
+router.get('/'      , PageController.home);
+router.get('/dogs'  , PageController.dogs);
+router.get('/cats'  , PageController.cats);
 router.get('/fishes', PageController.fishes);
 router.get('/search', SearchController.search);
 
